8d34b619f90b6bba8dea3357cabc6966f35f9833
ramraghu <raghu@raghu-Inspiron-5537>
2016-09-26 00:52:06 +0530

    Assignment1_2

---
+++ assignment1_1.java
@@ -0,0 +1,16 @@
+package learnJava;
+
+public class assignment1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 3;
+		int y = 4;
+		int z;
+		z = (x*x - y*y)/(x-y);
+		System.out.println(z);
+				
+	}
+
+}
